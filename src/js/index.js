c92f765938085d4323ca299e3fbc7054e40c0486
lidicev <lidice.villarreal001@mymdc.net>
2023-04-09 15:31:44 +0000

    comment

--- src/js/index.js
+++ src/js/index.js
@@ -10,3 +10,22 @@ import Home from "./component/home.jsx";
 
 //render your react application
 ReactDOM.render(<Home />, document.querySelector("#app"));
+
+
+
+// //import react into the bundle
+// import React from "react";
+// import ReactDOM from "react-dom";
+
+// //include bootstrap npm library into the bundle
+// import "bootstrap";
+
+// //include your index.scss file into the bundle
+// import "../styles/index.scss";
+// import "../styles/custom.css";
+
+// //import your own components
+// import Home from "./component/home.jsx";
+
+// //render your react application
+// ReactDOM.render(<Home />, document.querySelector("#app"));
